3abc6353554ab2c4aeca0429a9a075787a088e3d
shabelski89 <a.shabelsky@gmail.com>
2023-01-28 12:08:01 +0300

    [FIX] - multiline query support

--- src/pydb/dbclient.py
+++ src/pydb/dbclient.py
@@ -17,20 +17,43 @@ class SqlClient(cmd.Cmd):
         self.prompt = 'SQL> '
         self.delimiter = '=' * 40
         self.db = db
+        self._buffer = ''
+
+    def emptyline(self):
+        pass
 
     def default(self, line: str) -> None:
         self.do_execute(query=line)
 
     def do_execute(self, query: str) -> None:
-        time_begin_execute = time.time()
-        cursor = self.execute(query=query)
-        if cursor:
-            table: PrettyTable = from_db_cursor(cursor)
-            table.align = "l"
-            time_of_execute = time.time() - time_begin_execute
-            print(table)
-            print(f'Rows: {cursor.rowcount}')
-            print(f'Time: {time_of_execute:.2f}')
+        if self._check_query(query):
+            time_begin_execute = time.time()
+            cursor = self.execute(query=self._buffer)
+            self._clear_buffer()
+            if cursor:
+                table: PrettyTable = from_db_cursor(cursor)
+                table.align = "l"
+                time_of_execute = time.time() - time_begin_execute
+                print(table)
+                print(f'Rows: {cursor.rowcount}')
+                print(f'Time: {time_of_execute:.2f}')
+
+    def _check_query(self, line: str):
+        if not line.endswith(';'):
+            self._add_buffer(line)
+            return False
+        else:
+            if self._buffer:
+                self._add_buffer(line)
+            else:
+                self._buffer = line
+            return True
+
+    def _add_buffer(self, line: str):
+        self._buffer += ' ' + line.replace('\n',' ')
+
+    def _clear_buffer(self):
+        self._buffer = ''
 
     def help_execute(self):
         print(f'{self.delimiter}\nType: SELECT * FROM table;')
