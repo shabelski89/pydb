1b3c68cc7bbb0605b13d61e9ccef70eeb3058131
shabelski89 <a.shabelsky@gmail.com>
2024-02-02 18:49:19 +0300

    Повышена версия ПО

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pydb",
-    version="1.3.1",
+    version="1.3.2",
     author='Aleksandr Shabelsky',
     author_email='a.shabelsky@gmail.com',
     description="Light CLI Oracle, Postgres, Mysql",
